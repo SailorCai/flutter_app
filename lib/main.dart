3a87aa89081a29633a68f5af5ec451401ab9f058
蔡胜龙 <1187023819@qq.com>
2020-06-13 09:49:12 +0800

    scaffold脚手架

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
  * @Author: SailorCai
  * @Date: 2020-06-06 09:07:17
  * @LastEditors: SailorCai
- * @LastEditTime: 2020-06-06 13:51:12
+ * @LastEditTime: 2020-06-13 09:43:59
  * @FilePath: /flutter_app/lib/main.dart
  */
 import 'dart:convert';
@@ -24,121 +24,133 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'flutter class'),
+      home: FirstScreen(),
+      routes: <String, WidgetBuilder>{
+        '/first': (BuildContext context) => FirstScreen(),
+        '/second': (BuildContext context) => SecondScreen(),
+      },
+      initialRoute: '/first',
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+/* class FirstScreen extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text("firstScreen Title"),
+      ),
+      body: Center(child: Text("FirstScreen")),
+    );
+  }
+} */
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text("SecondScreen Title"),
+      ),
+      body: Center(child: Text("SecondScreen")),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class FirstScreen extends StatefulWidget {
+  final Widget child;
 
-  void _incrementCounter() {
+  FirstScreen({Key key, this.child}) : super(key: key);
+
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  final List<Widget> _children = [
+    ScreenWidget('银行'),
+    ScreenWidget('联系人'),
+    ScreenWidget('音乐'),
+  ];
+  int _currentIndex = 0;
+
+  void onTapAction(int selectedIndex) {
     setState(() {
-      _counter++;
+      _currentIndex = selectedIndex;
     });
-
-    httpFunction2();
-  }
-
-  //  https://randomuser.me/api/?results=30
-  httpFunction1() {
-    // http.post(url)
-
-    var url = 'https://randomuser.me/api/?results=30';
-    http.get(url).then((response) {
-      print("data: ${response}");
-    });
-  }
-
-  httpFunction2() async {
-    var httpClient = new HttpClient();
-    var url = 'https://randomuser.me/api/?results=30';
-    var request = await httpClient.getUrl(Uri.parse(url));
-    var response = await request.close();
-    var jsonString = await response.transform(utf8.decoder).join();
-    print("data: ${jsonString}");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+      appBar: new AppBar(
+        title: Text(
+          "First",
+          style: TextStyle(fontSize: 30),
         ),
-        // body: Center(
-        //     child: Image.asset(
-        //   'assets/images/avatar.jpeg',
-        //   width: 120,
-        //   height: 120,
-        // )),
-        body: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset(
-                  'assets/images/avatar.jpeg',
-                  width: 120,
-                  height: 120,
-                ),
-                Image.network(
-                  'https://media.licdn.cn/dms/image/C5603AQFGMwMdokkt8A/profile-displayphoto-shrink_200_200/0?e=1596672000&v=beta&t=pS4eQ3L0_IP7dl6vV9eqtb1vpF3QF5_u91rJ65sfvQw',
-                  width: 120,
-                  height: 120,
-                ),
-                new CircleAvatar(
-                  backgroundImage: new NetworkImage(
-                      'https://media.licdn.cn/dms/image/C5603AQFGMwMdokkt8A/profile-displayphoto-shrink_200_200/0?e=1596672000&v=beta&t=pS4eQ3L0_IP7dl6vV9eqtb1vpF3QF5_u91rJ65sfvQw'),
-                  radius: 60,
-                ),
-              ],
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              new FadeInImage.assetNetwork(
-                  placeholder: 'assets/images/avatar.jpeg',
-                  image:
-                      'https://media.licdn.cn/dms/image/C5603AQFGMwMdokkt8A/profile-displayphoto-shrink_200_200/0?e=1596672000&v=beta&t=pS4eQ3L0_IP7dl6vV9eqtb1vpF3QF5_u91rJ65sfvQw',
-                  width: 120,
-                  height: 120),
-              Image.asset(
-                'assets/images/avatar.jpeg',
-                width: 200,
-                height: 120,
-                fit: BoxFit.fitWidth,
-                // repeat: ImageRepeat.repeat,
-              ),
-            ]),
-            new Text(
-              '这是一个文本这是一个文本这是一个文本这是一个',
-              style: new TextStyle(
-                color: Colors.red,
-                fontSize: 20.0,
-                fontStyle: FontStyle.italic,
-              ),
-              textAlign: TextAlign.center,
-              // textDirection: TextDirection.rtl,
-              // softWrap: false,
-            ),
-            new Text.rich(
-              new TextSpan(
-                  text: '文本一',
-                  style: new TextStyle(color: Colors.blue, fontSize: 30.0),
-                  children: <TextSpan>[
-                    new TextSpan(
-                        text: '文本二',
-                        style: new TextStyle(color: Colors.red, fontSize: 20.0))
-                  ]),
-            ),
-          ],
-        ));
+        // leading: Text('左侧'),
+        // actions: [
+        //   Text('右侧'),
+        // ],
+        // centerTitle: false,
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: onTapAction,
+        items: [
+          new BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance),
+            title: Text("银行"),
+          ),
+          new BottomNavigationBarItem(
+            icon: Icon(Icons.contacts),
+            title: Text("联系人"),
+          ),
+          new BottomNavigationBarItem(
+            icon: Icon(Icons.library_music),
+            title: Text("音乐"),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: Center(
+          child: Text(
+            'Drawer',
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
+      ),
+    );
   }
+}
+
+class ScreenWidget extends StatelessWidget {
+  final String text;
+  // 构造函数
+  ScreenWidget(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 40),
+      ),
+    );
+  }
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: new AppBar(
+  //       title: Text(text),
+  //     ),
+  //     body: Center(
+  //       child: Text(
+  //         text,
+  //         style: TextStyle(fontSize: 40),
+  //       ),
+  //     ),
+  //   );
+  // }
 }
